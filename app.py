765c05da799c518670421ada1b4f98528a6561fc
mymyxtran <57696924+mymyxtran@users.noreply.github.com>
2021-02-14 12:27:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+import csv
+app = Flask(__name__)
 
+@app.route('/')
+def get_csv():
+    jsonArray = []   
+    with open('./data/store_data.csv', newline='') as csvfile:
+        datareader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        headers = reader.next()
+        for row in datareader[1:]:
+            jsonArray.append(dict(zip(headers, row)))
+    
+    return {"0" : jsonArray} ## return JSON
+        
+if __name__ == "__main__":
+    app.run()        
